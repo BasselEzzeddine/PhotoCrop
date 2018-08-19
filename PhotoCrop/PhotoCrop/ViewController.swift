2a324e5b8ac9b74609521e6cd7016c5d23ace6c3
Bassel Ezzeddine <bassel-maro@hotmail.com>
2018-08-19 12:32:16 +0200

    Refactoring

--- PhotoCrop/PhotoCrop/ViewController.swift
+++ PhotoCrop/PhotoCrop/ViewController.swift
@@ -17,13 +17,15 @@ class ViewController: UIViewController {
             scrollView.maximumZoomScale = 2
         }
     }
-    @IBOutlet weak var view_crop: UIView!
+    
     @IBOutlet weak var imageView: UIImageView!
     
-    // MARK: - UIViewController
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupUI()
+    @IBOutlet weak var view_crop: UIView! {
+        didSet {
+            view_crop.layer.cornerRadius = 5
+            view_crop.layer.borderWidth = 3
+            view_crop.layer.borderColor = UIColor.black.cgColor
+        }
     }
     
     // MARK: - Actions
@@ -35,13 +37,6 @@ class ViewController: UIViewController {
         let croppedImage = ImageCropHandler.sharedInstance.cropImage(imageToCrop, toRect: cropRect, viewWidth: imageView.frame.width, viewHeight: imageView.frame.height)
         imageView.image = croppedImage
         scrollView.zoomScale = 1
-    }
-    
-    // MARK: - Methods
-    func setupUI() {
-        view_crop.layer.cornerRadius = 5
-        view_crop.layer.borderWidth = 3
-        view_crop.layer.borderColor = UIColor.black.cgColor
     }
 }
 
